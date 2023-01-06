95d2b297edcd48b21ad19d8037fbf38d2d545293
Thamires Aluiza <thamiresaluizasantos@hotmail.com>
2023-01-06 08:04:51 -0300

    Trabalhando com map

--- src/main/kotlin/IntroArrays.kt
+++ src/main/kotlin/IntroArrays.kt
@@ -1,6 +1,17 @@
 import br.com.alura.arrays.teste.testaFuncoesArray
 
 fun main() {
+    val pedidos = mapOf(Pair(1, 20.0), Pair(2, 34.0), 3 to 50.0)
+    println(pedidos)
+    val pedido = pedidos[1]
+    pedido?.let {
+        println("pedido $it")
+    }
+    for (pedido: Map.Entry<Int, Double> in pedidos){
+        println("Número do pedido: ${pedido.key}")
+        println("Valor do pedido: ${pedido.value}")
+
+    }
 }
 
 
